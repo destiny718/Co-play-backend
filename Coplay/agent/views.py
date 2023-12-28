f12f71a52a0250b3f0e6138a823217f49a8a4bdd
张天祺 <zhangtq21@mails.tsinghua.edu.cn>
2023-12-28 16:51:56 +0800

    Feat: framework for API

--- Coplay/agent/views.py
+++ Coplay/agent/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
+def post_create_story(req: HttpRequest):
+    pass
+
+def get_story(req: HttpRequest):
+    pass
+
+def post_create_role(req: HttpRequest):
+    pass
+
+def post_update_role(req: HttpRequest):
+    pass
+
+def post_create_scene(req: HttpRequest):
+    pass
+
+def post_update_scene(req: HttpRequest):
+    pass
+
+def post_create_timestep(req: HttpRequest):
+    pass
+
+def post_create_interaction(req: HttpRequest):
+    pass
